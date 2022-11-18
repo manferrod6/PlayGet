eef343606b1cc0728d4ceefccd7457558545a735
Manuel Fernández Rodríguez <manferrod6@alum.us.es>
2022-11-18 17:58:38 +0100

    1.0 Primera versión
    Primera  versión de la  página a partir de una plantilla

---
+++ homepage/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'homepage/home.html')
+
+def index(request):
+    return render(request, 'homepage/index.html')
