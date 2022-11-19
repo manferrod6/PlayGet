55f95740584c8e97fb15b0423da85a0df1f4d9ff
Ahydul1 <ahydul1@gmail.com>
2022-11-19 15:37:53 +0100

    Task-102 Adición de modelos, BD y funcionalidad de mostrar productos finalizada con sus querys. Falta añadir productos a la cesta

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Producto(models.Model):
+    # id = models.UUIDField()
+    nombre   = models.TextField(max_length=200)
+    precio = models.PositiveIntegerField()
+    fabricante = models.TextField(max_length=200)
+    cantidad = models.PositiveIntegerField()
+    categoria =  models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
--- homepage/views.py
+++ homepage/views.py
@@ -1,12 +1,58 @@
 from django.shortcuts import render
+from .models import Producto
+from django.db.models import Q
 
 # Create your views here.
 
+categorias = [
+            'Aire libre',
+            'Videojuegos',
+            'Juegos de Mesa',
+            'Puzzles y Construcciones',
+            'Instrumentos Musicales',
+            'Electronicos',
+            ]
+    
 def home(request):
-    return render(request, 'homepage/home.html')
+    return render(request, 'homepage/index.html')
 
 def index(request):
     return render(request, 'homepage/index.html')
 
 def catalogo(request):
-    return render(request, 'homepage/catalogo.html')
+    tipoBusqueda = request.GET.get('tipo-busqueda','')
+    busqueda = request.GET.get('busqueda','')
+    filtros = request.GET.get('filtros','')
+    arr = filtros.split(';')
+    
+    if filtros == '':
+        if busqueda == '':
+                productos = Producto.objects.all()
+        else:
+            if tipoBusqueda == 'nombre':
+                productos = Producto.objects.filter(nombre__icontains = busqueda)
+            elif tipoBusqueda == 'fabricante':
+                productos = Producto.objects.filter(fabricante__icontains = busqueda) 
+    else:
+        if busqueda == '':
+            productos = Producto.objects.filter(categoria__in = arr)
+        else:
+            if tipoBusqueda == 'nombre':
+                productos = Producto.objects.filter(Q(nombre__icontains = busqueda) & Q(categoria__in = arr))
+            elif tipoBusqueda == 'fabricante':
+                productos = Producto.objects.filter(Q(fabricante__icontains = busqueda) & Q(categoria__in = arr))
+    
+
+
+
+
+    for p in productos:
+        p.cantidad = [*range(1, p.cantidad)]
+
+
+
+    return render(request, 'homepage/catalogo.html', 
+        {'productos': productos, 
+        'categorias': categorias, 
+        'busqueda': busqueda}
+    )
