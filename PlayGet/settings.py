9575af2796ec0620a3bc186ba0fe1657f5ac0d36
David Dana <davdancab@alum.us.es>
2022-12-07 22:54:12 +0100

    correcciones generales

--- PlayGet/settings.py
+++ PlayGet/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-s7(8kcxk6ttd+^rsney_3zay05$7n&=tf#o7@s(nc!coj^uepr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['PlayGet.pythonanywhere.com']
+ALLOWED_HOSTS = ['PlayGet.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
