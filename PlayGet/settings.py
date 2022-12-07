6b289402048f10458975a142a93d079be6462534
David Dana <daviddana61@gmail.com>
2022-12-07 21:21:39 +0100

    Update settings.py

--- PlayGet/settings.py
+++ PlayGet/settings.py
@@ -118,7 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
 
 STATIC_ROOT = "/home/PlayGet/PlayGet/staticfiles"
 # or, eg,
